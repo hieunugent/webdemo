96ff9545c522a250391edbaaab8243e0c106b5b8
Hieu Nguyen <hieunugent@gmail.com>
2019-12-18 21:04:56 -0800

    third commit with ejs

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 //jshint esversion:6
 
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
+app.set('view engine', 'ejs');
 
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname+ '/index.html');
+  // res.sendFile(__dirname+ '/index.html');
+  res.render("main");
 });
 
 app.listen(3000, function(){
